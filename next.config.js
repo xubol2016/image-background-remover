19001eeda71f41dbe6d3ed1927a2d5befc664f8b
xubol2016 <162607875+xubol2016@users.noreply.github.com>
2026-03-25 01:30:57 +0800

    Remove output and distDir from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
